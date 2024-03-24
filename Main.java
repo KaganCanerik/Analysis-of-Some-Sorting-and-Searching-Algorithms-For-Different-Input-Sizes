a8114cfc2cb26b71a166980b8e58693592333f4d
KaganCanerik <104866864+KaganCanerik@users.noreply.github.com>
2024-03-24 21:41:35 +0300

    Main.java

--- Main.java
+++ Main.java
@@ -9,23 +9,20 @@ class Main {
     static int[]  sizeArray = {500,1000,2000,4000,8000,16000,32000,64000,128000,250000};
     public static void main(String args[]) throws IOException {
 
-        // X axis data
-        int[] inputAxis = {512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 251282};
-
-        // Create sample data for linear runtime
-        double[][] yAxis = new double[2][10];
-        yAxis[0] = new double[]{512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 251282};
-        yAxis[1] = new double[]{300, 800, 1800, 3000, 7000, 15000, 31000, 64000, 121000, 231000};
-
         // Save the char as .png and show it
         String filePath = args[0];
         int[] fileDuration = ReadFromFile.readFile(filePath);
 
+        /**
+        * These are the Y axis for graphs which will be plotted at the end of the running time.*/
         double[][] timesAxisForInsertion = new double[3][10];
         double[][] timesAxisForMerge = new double[3][10];
         double[][] timesAxisForCounting = new double[3][10];
         double[][] timesAxisForSearching = new double[3][10];
-        ArrayList<ArrayList<Double>> timeList = new ArrayList<>();
+        /**
+         * This is the list which is hold the time duration of running of related algorithm.
+         */
+        ArrayList<Double> timeList = new ArrayList<>();
 
         doSortings(fileDuration,timeList,timesAxisForInsertion,"Insertion",0);
         doSortings(fileDuration,timeList,timesAxisForMerge,"Merge",30);
@@ -58,6 +55,16 @@ class Main {
 
 
     }
+    /***
+     * This the plotting graphic method. By using this method, we are able to comprehensive better to analyze of running time of related algorithm's execution.
+     * This method create a graph which include 3 plot and save as a png file.
+     @param title name of the png file.
+     @param xAxis X-axis of the graphic.
+     @param yAxis1 Y-axis of first plot
+     @param yAxis2 Y-axis of second plot
+     @param yAxis3 Y-axis of third plot
+     @param algoNameArray the array which includes names of algorithms that will be plotted at the graphic
+     */
 
     public static void showAndSaveChart(String title, int[] xAxis, double[] yAxis1, double[] yAxis2, double[] yAxis3,String[] algoNameArray) throws IOException {
         // Create Chart
@@ -71,9 +78,6 @@ class Main {
         chart.getStyler().setLegendPosition(Styler.LegendPosition.InsideNE);
         chart.getStyler().setDefaultSeriesRenderStyle(XYSeries.XYSeriesRenderStyle.Line);
 
-
-
-
         // Add a plot for a sorting algorithm
         chart.addSeries(algoNameArray[0], doubleX, yAxis1);
         chart.addSeries(algoNameArray[1], doubleX, yAxis2);
@@ -84,7 +88,17 @@ class Main {
         // Show the chart
         new SwingWrapper(chart).displayChart();
     }
-    public static void doSortings(int[] fileDuration, ArrayList<ArrayList<Double>> timeList,double[][] timeAxisArray,String algorithmName ,int initialIndex){
+    /**
+     * This method does the sorting operations through the different sizes of arrays.
+     * This method save the duration of sorting operation at Y-axis array which related is
+     * @param fileDuration the input array which will be constraint by using the specific index value at the sizeArray(is attribute of class).
+     * @param timeList the list which is stored the duration times of related algorithm's running time.
+     * @param timeAxisArray the two-dimensional array which is included Y-axis of related graphic.
+     * @param algorithmName name of related algorithmic operation.
+     * @param initialIndex index for algorithm of storing the times to timeAxisArray .
+     */
+
+    public static void doSortings(int[] fileDuration, ArrayList<Double> timeList,double[][] timeAxisArray,String algorithmName ,int initialIndex){
         for(int i=0; i<sizeArray.length; i++){
             int[] sortedArray =Experiments.experimentsSorting(sizeArray[i],fileDuration,"RANDOM",algorithmName,timeList);
 
@@ -100,7 +114,7 @@ class Main {
         int keyValue=initialIndex;
         for(int i=0; i<3; i++){
             while(initialIndex<timeList.size()){
-                timeAxisArray[i][indexOfAxis] = timeList.get(initialIndex).get(0);
+                timeAxisArray[i][indexOfAxis] = timeList.get(initialIndex);
                 initialIndex+=3;
                 indexOfAxis++;
             }
@@ -109,16 +123,23 @@ class Main {
             keyValue++;
         }
     }
-    public static void doSearchs(int[] fileDuration,ArrayList<ArrayList<Double>> timeList,double[][] timesAxisForSearching){
+    /**
+     * This method does the searching operations through the different sizes of arrays.
+     * This method save the duration of searching operation at Y-axis array which related is
+     * @param fileDuration the input array which will be constraint by using the specific index value at the sizeArray(is attribute of class).
+     * @param timeList the list which is stored the duration times of related algorithm's running time.
+     * @param timesAxisForSearching the two-dimensional array which is included Y-axis of related graphic.
+     */
+    public static void doSearchs(int[] fileDuration,ArrayList<Double> timeList,double[][] timesAxisForSearching){
         for(int i=0; i< sizeArray.length; i++){
 
             int[] copyArray = Arrays.copyOfRange(fileDuration ,0, sizeArray[i]);
-            timesAxisForSearching[0][i]=Experiments.experimentsSearching(sizeArray[i],copyArray,"Linear_searching",timeList );
+            timesAxisForSearching[0][i]=Experiments.experimentsSearching(copyArray,"Linear_searching",timeList );
             int[] sortedArray = Experiments.experimentsSorting(sizeArray[i],fileDuration,"RANDOM","Merge", timeList);
             int lastIndex = timeList.size()-1;
             timeList.remove(lastIndex);
-            timesAxisForSearching[1][i]=Experiments.experimentsSearching(sizeArray[i],sortedArray,"Linear_searching",timeList);
-            timesAxisForSearching[2][i]=Experiments.experimentsSearching(sizeArray[i],sortedArray,"Binary_search",timeList);
+            timesAxisForSearching[1][i]=Experiments.experimentsSearching(sortedArray,"Linear_searching",timeList);
+            timesAxisForSearching[2][i]=Experiments.experimentsSearching(sortedArray,"Binary_search",timeList);
         }
     }
 
